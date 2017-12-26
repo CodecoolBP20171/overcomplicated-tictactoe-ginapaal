1d50610fd23b2218adc0a43d6b41b47a833cd94c
Gina <paalginaa@gmail.com>
2017-12-26 22:07:25 +0100

    getting new funfact at every refresh

--- funfact-service/src/main/java/funfact/controller/FunFactController.java
+++ funfact-service/src/main/java/funfact/controller/FunFactController.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @RestController
 public class FunFactController {
 
@@ -14,7 +19,15 @@ public class FunFactController {
     private FunFactService funFactService;
 
     @GetMapping(value="/funfact")
-    public FunFactService funfact() {
-        return new FunFactService();
+    public FunFactService funfact() throws IOException {
+        List<String> quoteList = new ArrayList<>();
+        quoteList.add("ez");
+        quoteList.add("az");
+        quoteList.add("amaz");
+        quoteList.add("emez");
+        Random random = new Random();
+        int index = random.nextInt(quoteList.size());
+        String myFunFact = quoteList.get(index);
+        return new FunFactService(myFunFact);
     }
 }
--- funfact-service/src/main/java/funfact/model/FunFactService.java
+++ funfact-service/src/main/java/funfact/model/FunFactService.java
@@ -1,17 +1,42 @@
 package funfact.model;
 
+import funfact.FunFact;
 import org.springframework.stereotype.Component;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Component
 public class FunFactService {
 
+    private String value;
     private String funfact;
 
     public FunFactService() {
-        this.funfact = "This is a fun fact";
+    }
+
+    public FunFactService(String funfact) {
+        this.funfact = funfact;
     }
 
     public String getFunfact() {
         return funfact;
     }
+
+    public void setFunfact(String funfact) {
+        this.funfact = funfact;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
 }

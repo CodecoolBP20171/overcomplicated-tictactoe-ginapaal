55b111ff33465db43fd9550b107229b2c709c92f
Gina <paalginaa@gmail.com>
2017-12-28 23:00:08 +0100

    ai service skeleton

--- ticatactoe-ai-service/src/main/java/ai/controller/AiController.java
+++ ticatactoe-ai-service/src/main/java/ai/controller/AiController.java
@@ -1,8 +1,15 @@
 package ai.controller;
 
 
+import ai.model.AiModel;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class AiController {
+
 }
--- ticatactoe-ai-service/src/main/java/ai/model/AiModel.java
+++ ticatactoe-ai-service/src/main/java/ai/model/AiModel.java
@@ -1,8 +1,27 @@
 package ai.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AiModel {
 
     String gameState;
 
-    
+    public AiModel() {
+
+    }
+
+    public AiModel(String gameState) {
+
+    }
+
+    public String getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(String gameState) {
+        this.gameState = gameState;
+    }
+
 }

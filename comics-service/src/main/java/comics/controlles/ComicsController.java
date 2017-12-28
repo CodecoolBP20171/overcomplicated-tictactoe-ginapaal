56910c78908492a788235d09ee4f5719e74c9c7d
Gina <paalginaa@gmail.com>
2017-12-28 19:44:03 +0100

    comics service implementation

--- comics-service/src/main/java/comics/controlles/ComicsController.java
+++ comics-service/src/main/java/comics/controlles/ComicsController.java
@@ -2,19 +2,26 @@ package comics.controlles;
 
 
 import comics.model.ComicsModel;
+import comics.service.RemoteURLReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Random;
 
 @RestController
 public class ComicsController {
 
+    @Autowired
+    private RemoteURLReader remoteURLReader;
+
     @GetMapping("/comics")
-    public ComicsModel getComics() {
+    public ComicsModel getComics() throws IOException {
         Random random = new Random();
         int rand = random.nextInt(1929);
         String uri = "https://xkcd.com/" + rand + "/info.0.json";
-        return new ComicsModel(uri, rand);
+        String parsedURL = remoteURLReader.getHTML(uri);
+        return new ComicsModel(parsedURL, rand);
     }
 }
